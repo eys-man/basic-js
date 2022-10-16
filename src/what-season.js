c402b6203ba8d31947914550d5490b3b29822843
eys-man <kapuec2010@gmail.com>
2022-10-16 18:10:54 +0300

    feat: add tast what-season

--- src/what-season.js
+++ src/what-season.js
@@ -16,14 +16,50 @@ const { NotImplementedError } = require('../extensions/index.js');
   // remove line with error and write your code here
 // }
 
-function getSeason( date ) {
-  let season=''; // время года
+
+  //let year = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+
+function getSeason( date )
+{
+  if( !date )
+    return 'Unable to determine the time of year!';
+      
+  // https://overcoder.net/q/27069/%D0%BA%D0%B0%D0%BA-%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%B8%D1%82%D1%8C-%D1%8F%D0%B2%D0%BB%D1%8F%D0%B5%D1%82%D1%81%D1%8F-%D0%BB%D0%B8-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82-%D0%B4%D0%B0%D1%82%D0%BE%D0%B9
   
-  // проверить date на правильность
-   
-  
-  let now = new Date();
-}
+  if (!date.getTime) {
+    throw new Error('Invalid date!');
+  }
+  if (Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  }
+
+  let month = date.getMonth();
+
+  switch( month )
+  {
+    case 0:
+    case 1:
+    case 11:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+    default:
+      return 'Invalid date!';
+  };
+    
+  //throw new Error('Invalid date!');
+
+}   
 
 
 module.exports = {
