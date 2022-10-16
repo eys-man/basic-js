d48dc349eefa3ac0fdb107a782f04449418cc11a
eys-man <kapuec2010@gmail.com>
2022-10-16 20:52:18 +0300

    feat: add task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) 
+{
+  if( !Array.isArray(arr) ) throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+  let new_arr=[];
+
+  for( let i=0; i< arr.length; i++)
+  {
+    switch( arr[i] )
+    {
+      case '--discard-next':
+        i+=2; // пропустить следующий
+        break;
+      case '--discard-prev':
+        if( i > 0 ) new_arr.pop();
+        break;
+      case '--double-next':
+        if( i < arr.length-1 ) new_arr.push(arr[i+1]);
+        break;
+      case '--double-prev':
+        if( i > 0 ) new_arr.push( arr[i-1] );
+        break;
+      default: // если не управляющие последовательности, добавить символ
+        new_arr.push( arr[i] );
+        break;
+    }
+  }
+
+  return new_arr;
 }
 
 module.exports = {
